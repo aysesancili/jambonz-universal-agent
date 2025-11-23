f8f08fb4e6cc8e6c38a99c25169c493ba9d1fda3
aysesancili <109159941+aysesancili@users.noreply.github.com>
2025-11-23 16:24:47 +0300

    Refactor: Remove node-client-ws dependency, use native ws

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { createEndpoint } = require('@jambonz/node-client-ws');
+const { WebSocketServer } = require('ws');
 const { createServer } = require('http');
 const { logger } = require('./utils/logger');
 const sessionHandler = require('./session-handler');
@@ -14,28 +14,101 @@ const server = createServer((req, res) => {
   res.end();
 });
 
-// Jambonz Resmi Kütüphanesi ile Endpoint
-const makeService = createEndpoint({
-  server,
-  path: '/callControl'
+const wss = new WebSocketServer({ noServer: true });
+
+server.on('upgrade', (request, socket, head) => {
+  if (request.url === '/callControl') {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit('connection', ws, request);
+    });
+  } else {
+    socket.destroy();
+  }
 });
 
-// Yeni çağrı geldiğinde
-makeService.on('session:new', (session) => {
-  logger.info({ call_sid: session.call_sid }, 'New session received');
-  
-  // Hata yönetimi
-  session.on('close', (code, reason) => {
-    logger.info({ code, reason, call_sid: session.call_sid }, 'Session closed');
-  });
-  
-  session.on('error', (err) => {
-    logger.error({ err, call_sid: session.call_sid }, 'Session error');
-  });
+wss.on('connection', (ws) => {
+  logger.info('New WebSocket connection');
 
-  sessionHandler(session).catch((err) => {
-    logger.error({ err, call_sid: session.call_sid }, 'Session handler error');
-    session.close();
+  const session = {
+    call_sid: null,
+    direction: null,
+    from: null,
+    to: null,
+    ws: ws,
+    handlers: {},
+    
+    on: function(event, handler) {
+        this.handlers[event] = handler;
+        return this;
+    },
+
+    send: function(cmd) {
+        if (ws.readyState === ws.OPEN) {
+            ws.send(JSON.stringify(cmd));
+        }
+        return this;
+    },
+
+    answer: function() { return this; },
+    say: function(opts) { 
+        this.send({ type: 'command', command: 'say', data: opts }); 
+        return this; 
+    },
+    gather: function(opts) { 
+        this.send({ type: 'command', command: 'gather', data: opts }); 
+        return this; 
+    },
+    pause: function(opts) { 
+        this.send({ type: 'command', command: 'pause', data: opts }); 
+        return this; 
+    },
+    hangup: function() { 
+        this.send({ type: 'command', command: 'hangup' }); 
+        return this; 
+    },
+    close: function() { ws.close(); }
+  };
+
+  ws.on('message', (data) => {
+    try {
+      const msg = JSON.parse(data);
+
+      // ACK Gönder
+      if (msg.msgid) {
+        ws.send(JSON.stringify({
+            type: 'ack',
+            msgid: msg.msgid
+        }));
+      }
+
+      if (msg.type === 'session:new') {
+        session.call_sid = msg.data.call_sid;
+        session.direction = msg.data.direction;
+        session.from = msg.data.from;
+        session.to = msg.data.to;
+        
+        logger.info({ call_sid: session.call_sid }, 'Session New');
+        sessionHandler(session);
+      } 
+      else if (msg.type === 'verb:hook') {
+        // Hook ismini al (örn: /onSpeech)
+        // Jambonz hook'u tam URL olarak gönderebilir veya sadece path.
+        // Biz includes ile kontrol edelim.
+        const hookUrl = msg.data.hook;
+        
+        for (const [event, handler] of Object.entries(session.handlers)) {
+            if (hookUrl && hookUrl.includes(event)) {
+                handler(msg.data);
+            }
+        }
+      }
+      else if (msg.type === 'session:end') {
+          logger.info('Session End');
+      }
+
+    } catch (err) {
+      logger.error({ err }, 'WebSocket Message Error');
+    }
   });
 });
 
